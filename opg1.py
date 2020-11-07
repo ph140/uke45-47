6d84a43f616268392f2acf28d0ec62c336ed34ae
ph140 <petterh140@gmail.com>
2020-11-07 22:28:30 +0100

    opg 1

---
+++ opg1.py
@@ -0,0 +1,42 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def f(x):
+    return (np.e)**x - 5*x
+
+
+def g(x):
+    return x**2 - 4*x + 5
+
+
+def i(x):
+    return 2*x + 1
+
+
+def derivert(f, x, h):
+    return ((f(x+h)-f(x))/h)
+
+
+h = 0.001
+xmin = 2
+xmax = 5
+N = int((xmax - xmin)/h)+1
+xes = np.linspace(xmin, xmax, N)
+
+
+def showres(f):
+    func = f(xes)
+    df = derivert(f, xes, h)
+
+    plt.plot(xes, func, label='f(x)')
+    plt.plot(xes, df, label='f(x)')
+    plt.xlabel('x')
+    plt.grid('both')
+    plt.legend()
+    plt.show()
+
+
+showres(f)
+showres(g)
+showres(i)
