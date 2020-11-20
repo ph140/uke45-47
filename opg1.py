192a98aae0bc0c6ec5150912ec120dd96588afdf
ph140 <petterh140@gmail.com>
2020-11-21 00:48:21 +0100

    la til kommentarer

--- opg1.py
+++ opg1.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+# Definerer funskjonene som skal brukes
 def d(x):
     return (np.e)**x - 5*x
 
@@ -14,17 +15,24 @@ def i(x):
     return 2*x + 1
 
 
+# Funksjon for den deriverte
 def derivert(f, x, h):
     return ((f(x+h)-f(x))/h)
 
 
+# Bestemmer h-ferdi og definisjonsmengde
 h = 0.01
 xmin = 2
 xmax = 5
+
+# Bergner antall punkter vi akn ha med den gitte h-verdien
 N = int((xmax - xmin)/h)
+# Lager en array med punktene
 xes = np.linspace(xmin, xmax, N)
 
 
+# Funskjon som tar inn et funskjonsnavn som argument, og
+# plotter funksjonen og den deriverte av funksjonen.
 def showres(f):
     func = f(xes)
     df = derivert(f, xes, h)
@@ -37,6 +45,7 @@ def showres(f):
     plt.show()
 
 
+# Viser alle funksjonen hver for seg
 showres(d)
 showres(g)
 showres(i)
--- opg2.py
+++ opg2.py
@@ -2,12 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+# Laster datafilen og lager tome lister
 data = np.loadtxt('vogn.txt', float, skiprows=1)
 time = []
 speed1 = []
 speed2 = []
 
 
+# Lager funskjoner for den deriverte ved hjelp av newtons kvotient, og
+# Newton's symetriske kvotient.
 def derivert(x):
     return data[x+1][1] - data[x][1]
 
@@ -16,12 +19,15 @@ def symmetrisk_derivert(x):
     return (data[x+1][1] - data[x-1][1])/2
 
 
+# Går gjennom listen bortsett fra det første og siste elementet
+# Legger til derivert-verdier i sped1 og speed2, og legger til tidspunkt i time
 for i, j in enumerate(data[:-1]):
     if i:
         speed1.append(derivert(i))
         speed2.append(symmetrisk_derivert(i))
         time.append(j[0])
 
+# Plotter derivert og symmetrisk derivert
 plt.plot(time, speed1, 'g-')
 plt.plot(time, speed2, 'r-')
 plt.show()
