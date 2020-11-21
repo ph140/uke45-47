3cc91967ceec1f4f36a7f2df026300126e32fc21
ph140 <petterh140@gmail.com>
2020-11-21 18:51:01 +0100

    la til kommentarer

--- opg3.py
+++ opg3.py
@@ -6,6 +6,8 @@ def f(x):
     return (1/sqrt(2*pi)) * e**((-x**2)/2)
 
 
+# Returnerer det samlede arealet mellom funksjonen f og x-aksen, fra a til b.
+# Ved bruk av rektangelmetoden
 def rektangelmetoden(f, a, b, N, offset=0.5):
     S = 0
     dx = (b-a)/N
@@ -19,6 +21,8 @@ def rektangelmetoden(f, a, b, N, offset=0.5):
     return S
 
 
+# Returnerer det samlede arealet mellom funksjonen f og x-aksen, fra a til b.
+# Ved bruk av trapesmetoden
 def trapesmetoden(f, a, b, N):
     S = 0
     dx = (b-a)/N
@@ -33,13 +37,20 @@ def trapesmetoden(f, a, b, N):
     return S
 
 
+# Lager en fil, eller sletter alt som er i den fra for, slik at den er blank.
 with open('normalfordeling.txt', 'w') as _:
     pass
 
 
 def res(h):
+    '''
+    Bruker både rektangelmetoden og trapesemetoden til å finne arealet fra
+    -1, 1 -2,2 og -3, 3, med funskjonen f. Bruker steglengde h.
+    Og skriver ut resultatene fra utregningen i filen normalfordeling.txt,
+    runder også av til bare to desimaler.
+    '''
     for k in range(1, 4):
-        N = int((k*2)/h)+1
+        N = int((k*2)/h)
         rek = round(rektangelmetoden(f, -k, k, N), 2)
         trapes = round(trapesmetoden(f, -k, k, N), 2)
         with open('normalfordeling.txt', 'a') as file:
@@ -52,5 +63,6 @@ def res(h):
         # print(f"Trapesmetoden: {trapes}\n")
 
 
+# Kjører h=0.1 og h=0.00001 gjennom resfunksjonen
 res(0.1)
 res(0.00001)
